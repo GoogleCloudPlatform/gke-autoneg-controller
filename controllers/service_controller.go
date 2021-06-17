8eec3a3805a6313bbc6750d6faf45fca0266a99e
Oliver Soell <osoell@google.com>
2021-06-17 12:21:25 +0200

    Clean up RBAC gen, make rules more restrictive

--- controllers/service_controller.go
+++ controllers/service_controller.go
@@ -39,8 +39,9 @@ type ServiceReconciler struct {
 	Log      logr.Logger
 }
 
-// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;update;patch
 // +kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 func (r *ServiceReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
