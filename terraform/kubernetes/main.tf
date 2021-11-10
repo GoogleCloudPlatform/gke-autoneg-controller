395edfe02e4eed22eb8b943a6379c175e4f8eaae
Oliver Soell <osoell@google.com>
2021-11-10 08:46:24 +0100

    Re-adding explicit declaration of automount_service_account_token

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -298,6 +298,7 @@ resource "kubernetes_deployment" "deployment_autoneg_controller_manager" {
 
       spec {
         service_account_name             = kubernetes_service_account.service_account.metadata[0].name
+        automount_service_account_token  = true
         termination_grace_period_seconds = 10
         node_selector = {
           "iam.gke.io/gke-metadata-server-enabled" = "true"
