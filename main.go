d8065c92ba4d86c44d221a59dedb5d676a412ccf
Oliver Soell <osoell@google.com>
2022-03-22 11:23:48 +0100

    Bumping version for 0.9.6 release

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 	// +kubebuilder:scaffold:imports
 )
 
-const useragent = "google-pso-tool/gke-autoneg-controller/0.9.6-dev"
+const useragent = "google-pso-tool/gke-autoneg-controller/0.9.6"
 
 var (
 	scheme   = runtime.NewScheme()
