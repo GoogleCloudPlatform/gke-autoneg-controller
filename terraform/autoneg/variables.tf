644e0079331dae0859a40e6d63fbeeb014952c20
Jason elsocht <jason.elsocht@idloom.com>
2024-03-28 09:21:15 +0100

    sort variables and outputs

--- terraform/autoneg/variables.tf
+++ terraform/autoneg/variables.tf
@@ -1,28 +1,13 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-variable "project_id" {
-  type        = string
-  description = "GCP project ID"
-}
-
 variable "controller_image" {
   type        = string
   description = "Autoneg controller container image"
   default     = "ghcr.io/googlecloudplatform/gke-autoneg-controller/gke-autoneg-controller:v1.0.0"
+}
+
+variable "custom_role_add_random_suffix" {
+  type        = bool
+  description = "Sets random suffix at the end of the IAM custom role id"
+  default     = false
 }
 
 variable "image_pull_policy" {
@@ -37,6 +22,23 @@ variable "kube_rbac_proxy_image" {
   default     = "gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0"
 }
 
+variable "priority_class_name" {
+  description = "Pod's PriorityClass name"
+  type        = string
+  default     = null
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "service_account_id" {
+  description = "Service account id to be created"
+  default     = "autoneg"
+  type        = string
+}
+
 variable "workload_identity" {
   description = "Workload identity configuration"
   type = object({
@@ -47,22 +49,4 @@ variable "workload_identity" {
     namespace       = "autoneg-system"
     service_account = "autoneg"
   }
-}
-
-variable "custom_role_add_random_suffix" {
-  type        = bool
-  description = "Sets random suffix at the end of the IAM custom role id"
-  default     = false
-}
-
-variable "service_account_id" {
-  description = "Service account id to be created"
-  default     = "autoneg"
-  type        = string
-}
-
-variable "priority_class_name" {
-  description = "Pod's PriorityClass name"
-  type        = string
-  default     = null
 }
--- terraform/kubernetes/variables.tf
+++ terraform/kubernetes/variables.tf
@@ -1,24 +1,3 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-variable "project_id" {
-  type        = string
-  description = "GCP project ID"
-}
-
 variable "controller_image" {
   type        = string
   description = "Autoneg controller container image"
@@ -36,15 +15,26 @@ variable "kube_rbac_proxy_image" {
   default     = "gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0"
 }
 
-variable "service_account_email" {
-  type        = string
-  description = "Autoneg service account email"
-}
-
 variable "namespace" {
   type        = string
   description = "Autoneg namespace"
   default     = "autoneg-system"
+}
+
+variable "priority_class_name" {
+  description = "Pod's PriorityClass name"
+  type        = string
+  default     = null
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "service_account_email" {
+  type        = string
+  description = "Autoneg service account email"
 }
 
 variable "service_account_id" {
@@ -63,10 +53,4 @@ variable "workload_identity" {
     namespace       = "autoneg-system"
     service_account = "autoneg"
   }
-}
-
-variable "priority_class_name" {
-  description = "Pod's PriorityClass name"
-  type        = string
-  default     = null
 }
